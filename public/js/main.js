f121970ee48a72acd6b6b42e75d391cb1991dc07
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:39:57 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+let phrase ="Bonjour tout le monde"
+// console.log(phrase.length);
+
+
+// console.log(phrase.trim());
+
+// console.log(phrase.length);
+
+
+// console.log(phrase.slice(20));
+
+
+// console.log(phrase.substring(8));
+
+// let phrasebi = "tout le monde"
+// console.log(phrasebi.replace("tout", "Bonjour tout"));
+
+// console.log(phrase.substr(-21, 7));
+
+
+
+// let phrase2 = "Ceci est une variable de type number !"
+// console.log(phrase2.replace("number", "string "));
+// let phrase3 = "Ceci est une variable de type NuMbEr ! Les numbers c'est trop bien Number numBer numbeR"
+// phrase3 =  phrase3.replace(/number/ig, "number");
+// phrase3= phrase3.toLocaleLowerCase();
+// console.log(phrase3);
+
+
